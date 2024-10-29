ad4c0301b3159873f8e595327e2ef423a3426a34
bitsforbricks <59959540+bitsforbricks@users.noreply.github.com>
2024-10-29 11:55:40 +0100

    Support parsing date headers with duplicate timezone values

--- parsemail.go
+++ parsemail.go
@@ -580,10 +580,14 @@ func (hp *headerParser) parseTime(s string) (t time.Time) {
 		"Mon, 2 Jan 2006 15:04:05 -0700",
 		time.RFC1123Z + " (MST)",
 		"Mon, 2 Jan 2006 15:04:05 -0700 (MST)",
+		time.RFC1123Z + " (GMT-07:00)", // include additional tz
+		"Mon, 2 Jan 2006 15:04:05 -0700 (GMT-07:00)", // include additional tz
 		time.RFC1123Z[5:], // omit dow
 		"2 Jan 2006 15:04:05 -0700",
 		time.RFC1123Z[5:] + " (MST)", // omit dow
 		"2 Jan 2006 15:04:05 -0700 (MST)",
+		time.RFC1123Z[5:] + " (GMT-07:00)", // include additional tz and omit dow
+		"2 Jan 2006 15:04:05 -0700 (GMT-07:00)", // include additional tz
 	}
 
 	for _, format := range formats {
